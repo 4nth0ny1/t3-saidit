594305e656823363914610eafdf26f7b1018b853
Anthony Catullo <anthonycatullo@oatslam.local>
2023-06-10 12:37:59 -0500

    removed link from comment

--- src/components/comments/CommentItem.tsx
+++ src/components/comments/CommentItem.tsx
@@ -1,5 +1,4 @@
 import type { Comment } from "../../types";
-import Link from "next/link";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 
@@ -10,14 +9,13 @@ type CommentProps = {
 dayjs.extend(relativeTime);
 
 export function CommentItem({ comment }: CommentProps) {
-  const { id, message, createdAt } = comment;
+  const { message, createdAt } = comment;
 
   return (
     <div className="flex flex-col border-b p-4">
       <div className="flex flex-row justify-between ">
-        <Link href={`/topic/post/${id}`}>
-          <h2 className="text-xl">{message}</h2>
-        </Link>
+        <h2 className="text-xl">{message}</h2>
+
         <span className="font-thin italic">{` Posted ${dayjs(
           createdAt
         ).fromNow()}`}</span>
