c33870386d6942ae1811fdc8d8a8e399b30fb1d3
Anthony Catullo <anthonycatullo@oatslam.local>
2023-06-10 14:22:57 -0500

    added Hero to topicId page

--- src/pages/topic/[topicId].tsx
+++ src/pages/topic/[topicId].tsx
@@ -15,14 +15,21 @@ const SingleTopicPage: NextPage = () => {
   if (isError) return <div>Something went wrong</div>;
 
   return (
-    <div className="flex w-full flex-row text-white">
-      <div className="h-[100vh] w-[15%] border-r-2 p-10">
-        <h2 className="text-3xl">{data?.name}</h2>
-        <p>{data?.description}</p>
+    <div className="w-full text-white">
+      <div className="hero h-[400px]">
+        <div className="hero-content flex-col lg:flex-row">
+          {/* <img
+            src="/images/stock/photo-1635805737707-575885ab0820.jpg"
+            className="max-w-sm rounded-lg shadow-2xl"
+          /> */}
+          <div>
+            <h1 className="text-5xl font-bold ">{data?.name}</h1>
+            <p className="py-6 ">{data?.description}</p>
+            <button className="btn-primary btn">Get Started</button>
+          </div>
+        </div>
       </div>
-      <div className="w-[85%] py-10">
-        <PostList topicId={topicId} />
-      </div>
+      <PostList topicId={topicId} />
     </div>
   );
 };
