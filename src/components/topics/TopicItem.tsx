c5cadfc64fa1a2ef5dbe4acaa0dd8871e49e747c
Anthony Catullo <anthonycatullo@oatslam.local>
2023-06-12 22:51:30 -0500

    deleteTopic with hover on topicItem

--- src/components/topics/TopicItem.tsx
+++ src/components/topics/TopicItem.tsx
@@ -1,5 +1,8 @@
 import type { Topic } from "../../types";
 import Link from "next/link";
+import { AiFillDelete } from "react-icons/ai";
+import { useState } from "react";
+import { api } from "../../utils/api";
 
 type TopicProps = {
   topic: Topic;
@@ -7,12 +10,30 @@ type TopicProps = {
 
 export function TopicItem({ topic }: TopicProps) {
   const { id, name } = topic;
+  const [hover, setHover] = useState(false);
+
+  const ctx = api.useContext();
+
+  const { mutate: deleteTopic } = api.topic.deleteTopic.useMutation({
+    onSettled: async () => {
+      await ctx.topic.getAllTopics.invalidate();
+    },
+  });
 
   return (
-    <div className="flex flex-row justify-center p-4">
+    <div
+      className="flex flex-row justify-center p-4"
+      onMouseEnter={() => setHover(!hover)}
+      onMouseLeave={() => setHover(!hover)}
+    >
       <Link href={`/topic/${id}`}>
-        <button className="btn text-black">{name}</button>{" "}
+        <button className="btn text-black">{name}</button>
       </Link>
+      {hover && (
+        <button onClick={() => deleteTopic(id)} className="pl-1 text-red-500">
+          <AiFillDelete />
+        </button>
+      )}
     </div>
   );
 }
