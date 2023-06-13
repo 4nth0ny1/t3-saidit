431a3d3a2f6340a548f257e36c939ee717400706
Anthony Catullo <anthonycatullo@oatslam.local>
2023-06-13 10:21:15 -0500

    refactor Hero again

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,27 +13,25 @@ export function Hero({ post, topic }: HeroProps) {
   return (
     <div className="hero h-[400px]">
       <div className="hero-content flex-col lg:flex-row ">
-        <div>
-          {post && (
-            <div>
-              <h2 className="text-5xl font-bold ">{post?.title}</h2>
-              <p className="py-6 ">{post?.content}</p>
-              <span className="font-thin italic">{` Created ${dayjs(
-                post?.createdAt
-              ).fromNow()}`}</span>
-            </div>
-          )}
+        {post && (
+          <div>
+            <h2 className="text-5xl font-bold ">{post?.title}</h2>
+            <p className="py-6 ">{post?.content}</p>
+            <span className="font-thin italic">{` Created ${dayjs(
+              post?.createdAt
+            ).fromNow()}`}</span>
+          </div>
+        )}
 
-          {topic && (
-            <div>
-              <h2 className="text-5xl font-bold ">{topic?.name}</h2>
-              <p className="py-6 ">{topic?.description}</p>
-              <span className="font-thin italic">{` Created ${dayjs(
-                topic?.createdAt
-              ).fromNow()}`}</span>
-            </div>
-          )}
-        </div>
+        {topic && (
+          <div>
+            <h2 className="text-5xl font-bold ">{topic?.name}</h2>
+            <p className="py-6 ">{topic?.description}</p>
+            <span className="font-thin italic">{` Created ${dayjs(
+              topic?.createdAt
+            ).fromNow()}`}</span>
+          </div>
+        )}
       </div>
     </div>
   );
